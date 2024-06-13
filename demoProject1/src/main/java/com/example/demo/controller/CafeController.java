bb9ced5b53c8588913c9f52a3eb33542f3e0f084
cnu0729 <smile-shinwoo@naver.com>
2024-06-13 14:53:47 +0900

    controller dto footer

--- demoProject1/src/main/java/com/example/demo/controller/CafeController.java
+++ demoProject1/src/main/java/com/example/demo/controller/CafeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.dto.CafeDTO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -17,6 +19,7 @@ public class CafeController {
 	// @RequestParam 이용해서 blog-index.html 파일에 작성한
 	// cafeCommentName, cafeCommentOpinion 작성하기
 	// return "redirect:/cafe-index"
+	/*
 	@PostMapping("cafe-index")
 	public String cafeMainComment(@RequestParam("cafeCommentName") String cafeCommentName,
 							  @RequestParam("cafeCommentOpinion") String cafeCommentOpinion
@@ -27,6 +30,19 @@ public class CafeController {
 		log.debug("cafeCommentName : " +cafeCommentName);
 		log.debug("cafeCommentOpinion : " +cafeCommentOpinion);
 		
-		return "redirect:/cafe/cafe-index";
+		return "redirect:/cafe/cafe-index"; ModelAttribute 받아오기
 	}
+	*/
+	@PostMapping("cafe_index")
+	public String cafeIndexComment(CafeDTO cafeDTO) {
+		CafeDTO cd = new CafeDTO();
+		cd.getCafeCommentName();
+		cd.getCafeCommentOpinion();
+		
+		cd.setCafeCommentName("넣어줄값 = 나중에 db 연결에서 전달해줄 값");
+		cd.setCafeCommentOpinion("넣어줄값 = 나중에 db 연결에서 전달해줄 값");
+		
+		return "";
+	}
+	
 }
---
+++ demoProject1/src/main/java/com/example/demo/model/dto/CafeDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class CafeDTO {
+
+	private String cafeCommentName;
+	private String cafeCommentOpinion;
+	
+}
