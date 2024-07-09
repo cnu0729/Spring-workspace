dd02339c13ac7eeaed1ec7ae54b299795814191e
cnu0729 <smile-shinwoo@naver.com>
2024-07-09 17:43:16 +0900

    ajex springtestdir

---
+++ SpringSelectResultMap/src/main/java/com/kh/test/SpringTestDir1Application.java
@@ -0,0 +1,13 @@
+package com.kh.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDir1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestDir1Application.class, args);
+	}
+
+}
