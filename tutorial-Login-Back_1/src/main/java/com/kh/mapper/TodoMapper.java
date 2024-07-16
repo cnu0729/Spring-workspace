e7373f3cb08db56c7b0d9abd39aeb1f4ce1ba3e0
cnu0729 <smile-shinwoo@naver.com>
2024-07-16 14:04:16 +0900

    login react

--- tutorial-Login-Back_1/src/main/java/com/kh/mapper/TodoMapper.java
+++ tutorial-Login-Back_1/src/main/java/com/kh/mapper/TodoMapper.java
@@ -7,6 +7,8 @@ import com.kh.dto.TodoMember;
 @Mapper
 public interface TodoMapper {
 	int idCheck(String id);
-	
 	int signup(TodoMember todoMember);
+	
+	TodoMember login(TodoMember member); // id pw 만 써도 되고, TodoMember 모두 써도됨
+	//TodoMember login(String id, String pw); // id pw 만 가져와서 로그인 하기
 }
